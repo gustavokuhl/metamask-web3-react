b686a272d7a56c4ef26c6497af0b080dbc865eda
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-08-29 21:05:53 -0300

    transfer dapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {getBalance, transfer} from './MetaMaskService';
+
 
 function App() {
+
+  const [addressFrom, setAddressFrom] = useState("0x6ad1bb9de62b004aecdab504f5c6ec18f494d08d");
+  const [addressTo, setAddressTo] = useState("0xaf83f96944ceda6052AB0B9F78adFEb7661A6Ed7");
+  const [quantity, setQuantity] = useState("0");
+  const [message, setMessage] = useState("");
+
+  function getBalanceClick(){
+    getBalance(addressFrom)
+      .then(balance => setMessage(balance))
+  }
+
+  function transferClick(){
+    transfer(addressFrom,
+              addressTo,
+              quantity)
+              .then(tx => setMessage(tx))
+              .catch(err => setMessage(err.message));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Wallet: <input type='text' value={addressFrom} onChange={(event) => setAddressFrom(event.target.value)} />
+      <button onClick={getBalanceClick}>getBalance</button>
+      <br />
+      To: <input type='text' value={addressTo} onChange={(event) => setAddressTo(event.target.value)} />
+      <br />
+      Quantity: <input type='text' value={quantity} onChange={(event) => setQuantity(event.target.value)} />
+      <button onClick={transferClick}>transfer</button>
+      <br />
+      {message}
     </div>
   );
 }
